e002a77049fcc4d7b1fd2fd7018bd0f426d93d0c
farleih <farleih@38162229-2249-0410-8976-db8ea7608f86>
2008-05-27 00:29:31 +0000

    Nova versão 2.0 beta da libegl Biblioteca separada em .h/.cpp para viabilizar o uso com multiplos .cpps (biblioteca estática) Nova estrutura de diretórios (por enquanto somente para Visual Studio 2005/2008) Pequenas melhorias na libegl internamente: - egl_texto() -> parametros novos para informar a cor do texto

--- library_version.h
+++ library_version.h
@@ -1,8 +1,9 @@
+#pragma once
 
 #if _MSC_VER >= 1400
    // this is Visual C++ 2005
-	#pragma comment(lib, "..//..//allegro-msvc80-4.2.2//lib//alleg.lib")
-	#include ".//allegro-msvc80-4.2.2//include//allegro.h"
+	#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
+	#include "allegro-msvc80-4.2.2//include//allegro.h"
 #elif _MSC_VER >= 1310
    // this is Visual c++ .NET 2003 com Service Pack 1
 	#pragma comment(lib, "..//..//allegro-msvc71-4.2.2//lib//alleg.lib")
