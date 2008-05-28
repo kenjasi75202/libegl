ff889ca1feb72a90ea7ce44c8140d8e82a7c5bb1
farleih <farleih@38162229-2249-0410-8976-db8ea7608f86>
2008-05-28 13:34:19 +0000

    pequenas correções na library_version.h

--- library_version.h
+++ library_version.h
@@ -6,10 +6,10 @@
 	#include "allegro-msvc80-4.2.2//include//allegro.h"
 #elif _MSC_VER >= 1310
    // this is Visual c++ .NET 2003 com Service Pack 1
-	#pragma comment(lib, "..//..//allegro-msvc71-4.2.2//lib//alleg.lib")
+	#pragma comment(lib, "..//egl//allegro-msvc71-4.2.2//lib//alleg.lib")
 	#include "allegro-msvc71-4.2.2//include//allegro.h"
 #else
    // this is Visual C++ .NET 2003 sem Service Pack 1
-	#pragma comment(lib, "..//..//allegro-msvc70-4.2.2//lib//alleg.lib")
+	#pragma comment(lib, "..//egl//allegro-msvc70-4.2.2//lib//alleg.lib")
 	#include "allegro-msvc70-4.2.2//include//allegro.h"
 #endif 
