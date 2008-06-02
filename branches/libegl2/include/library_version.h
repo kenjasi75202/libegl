f3a5cfc84d0664581896fdbdd1b1bd28a4d77255
farleih <farleih@38162229-2249-0410-8976-db8ea7608f86>
2008-06-02 23:21:33 +0000

    re-estruturação da árvore de diretórios e migração de libegl para biblioteca estática ainda sem o MERGE com o código do Tulio...

--- branches/libegl2/include/library_version.h
+++ branches/libegl2/include/library_version.h
@@ -2,17 +2,20 @@
 
 #if _MSC_VER >= 1400
    // this is Visual C++ 2005
+	#pragma message("added libs:alleg,libegl (VS2005+)")
 	#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//alpng_win32.lib")
+	#pragma comment(lib, "..//egl//lib//libegl.lib")
 	#include "allegro-msvc80-4.2.2//include//allegro.h"
 #elif _MSC_VER >= 1310
    // this is Visual c++ .NET 2003 com Service Pack 1
+	#pragma message("added libs:alleg,libegl (VS2003 SP1)")
 	#pragma comment(lib, "..//egl//allegro-msvc71-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//alpng_win32.lib")
+	#pragma comment(lib, "..//egl//lib//libegl.lib")
 	#include "allegro-msvc71-4.2.2//include//allegro.h"
 #else
    // this is Visual C++ .NET 2003 sem Service Pack 1
+	#pragma message("added libs:alleg,libegl (VS2003)")
 	#pragma comment(lib, "..//egl//allegro-msvc70-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//alpng_win32.lib")
+	#pragma comment(lib, "..//egl//lib//libegl.lib")
 	#include "allegro-msvc70-4.2.2//include//allegro.h"
 #endif 
