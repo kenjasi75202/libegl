36183ff57c58a1d5d1c44fea504c6e0324fce3a5
farleih <farleih@38162229-2249-0410-8976-db8ea7608f86>
2008-09-10 16:40:06 +0000

    Alteracao ampla na library_version.h para selecionar bibliotecas em modo DEBUG ou RELEASE.
    Melhorias nos projetos da libegl para VS2005 e VS2008.
    
    Binarios atualizados para VS2005 e VS2008.

--- include/library_version.h
+++ include/library_version.h
@@ -1,26 +1,52 @@
 #pragma once
+// libegl2: Cabecalho para configuracao automatica das bibliotecas no Visual Studio
 #if _MSC_VER >= 1500
    // this is Visual C++ 2008
-	#pragma message("added libs:alleg,libegl (VS2008+)")
-	#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//NET2008//libegl.lib")
-	#include "..//allegro-msvc80-4.2.2//include//allegro.h"
+	#include "..//allegro-msvc80-4.2.2//include//allegro.h"	
+	#ifndef _DEBUG
+		#pragma message("RELEASE: added libs:alleg,libegl (VS2008+)")
+		#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2008//libegl.lib")
+	#else
+		#pragma message("DEBUG: added libs:alleg,libegl_d (VS2008+)")
+		#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2008//libegl_d.lib")
+	#endif	
 #elif _MSC_VER >= 1400
    // this is Visual C++ 2005
-	#pragma message("added libs:alleg,libegl (VS2005)")
-	#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//NET2005SP1//libegl.lib")
-	#include "..//allegro-msvc80-4.2.2//include//allegro.h"
+	#include "..//allegro-msvc80-4.2.2//include//allegro.h"	
+	#ifndef _DEBUG
+		#pragma message("RELEASE: added libs:alleg,libegl (VS2005)")		
+		#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2005SP1//libegl.lib")
+	#else
+		#pragma message("DEBUG: added libs:alleg,libegl_d (VS2005)")
+		#pragma comment(lib, "..//egl//allegro-msvc80-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2005SP1//libegl_d.lib")
+	#endif
 #elif _MSC_VER >= 1310
    // this is Visual c++ .NET 2003 com Service Pack 1
-	#pragma message("added libs:alleg,libegl (VS2003 SP1)")
-	#pragma comment(lib, "..//egl//allegro-msvc71-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//NET2003SP1//libegl.lib")
-	#include "..//allegro-msvc71-4.2.2//include//allegro.h"
+	#include "..//allegro-msvc71-4.2.2//include//allegro.h"	
+	#ifndef _DEBUG
+		#pragma message("RELEASE: added libs:alleg,libegl (VS2003 SP1)")
+		#pragma comment(lib, "..//egl//allegro-msvc71-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2003SP1//libegl.lib")
+	#else
+		#pragma message("DEBUG: added libs:alleg,libegl_d (VS2003 SP1)")
+		#pragma comment(lib, "..//egl//allegro-msvc71-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2003SP1//libegl_d.lib")
+	#endif
 #else
    // this is Visual C++ .NET 2003 sem Service Pack 1
-	#pragma message("added libs:alleg,libegl (VS2003-)")
-	#pragma comment(lib, "..//egl//allegro-msvc70-4.2.2//lib//alleg.lib")
-	#pragma comment(lib, "..//egl//lib//NET2003//libegl.lib")
 	#include "..//allegro-msvc70-4.2.2//include//allegro.h"
+	#ifndef _DEBUG
+		#pragma message("RELEASE: added libs:alleg,libegl (VS2003-)")
+		#pragma comment(lib, "..//egl//allegro-msvc70-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2003//libegl.lib")
+	#else
+		#pragma message("DEBUG: added libs:alleg,libegl (VS2003-)")
+		#pragma comment(lib, "..//egl//allegro-msvc70-4.2.2//lib//alleg.lib")
+		#pragma comment(lib, "..//egl//lib//NET2003//libegl_d.lib")
+	#endif
+
 #endif 
